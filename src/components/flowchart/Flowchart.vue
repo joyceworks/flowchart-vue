b52b8d499a8cf456579e39a1347245ed4269168e
izsl <iamppz@hotmail.com>
2020-05-06 23:53:41 +0800

    refactor: move default render to render.js

--- src/components/flowchart/Flowchart.vue
+++ src/components/flowchart/Flowchart.vue
@@ -20,10 +20,11 @@
     between,
     distanceOfPointToLine,
     getEdgeOfPoints,
-    pointRectangleIntersection, roundTo20,
+    pointRectangleIntersection,
   } from '../../utils/math';
   import Vue from 'vue';
   import VueI18n from 'vue-i18n';
+  import render from './render';
 
   Vue.use(VueI18n);
 
@@ -74,83 +75,8 @@
       },
       render: {
         type: Function,
-        default: function(g, node, isSelected) {
-          let borderColor = isSelected ? '#666666' : '#bbbbbb';
-          if (node.type !== 'start' && node.type !== 'end') {
-            // title
-            g.append('rect').
-                attr('x', node.x).
-                attr('y', node.y).
-                attr('stroke', borderColor).
-                attr('class', 'title').
-                style('height', '20px').
-                style('fill', '#f1f3f4').
-                style('stroke-width', '1px').
-                style('width', node.width + 'px');
-            g.append('text').
-                attr('x', node.x + 4).
-                attr('y', node.y + 15).
-                attr('class', 'unselectable').
-                text(() => node.name).
-                each(function wrap() {
-                  let self = d3.select(this),
-                      textLength = self.node().getComputedTextLength(),
-                      text = self.text();
-                  while (textLength > (node.width - 2 * 4) && text.length > 0) {
-                    text = text.slice(0, -1);
-                    self.text(text + '...');
-                    textLength = self.node().getComputedTextLength();
-                  }
-                });
-          }
-          // body
-          let body = g.append('rect').attr('class', 'body');
-          body.style('width', node.width + 'px').style('fill', 'white').style('stroke-width', '1px');
-          if (node.type !== 'start' && node.type !== 'end') {
-            body.attr('x', node.x).attr('y', node.y + 20);
-            body.style('height', roundTo20(node.height - 20) + 'px');
-          } else {
-            body.attr('x', node.x).attr('y', node.y).classed(node.type, true).attr('rx', 30);
-            body.style('height', roundTo20(node.height) + 'px');
-          }
-          body.attr('stroke', borderColor);
-
-          // body text
-          let text = node.type === 'start'
-              ? i18n.t('message.start')
-              : (node.type === 'end' ? i18n.t('message.end') : (
-                      (!node.approvers || node.approvers.length === 0)
-                          ? i18n.t('message.noApprover')
-                          : (
-                              node.approvers.length > 1
-                                  ? `${node.approvers[0].name + i18n.t('message.etc')}`
-                                  : node.approvers[0].name
-                          )
-                  )
-              );
-          let bodyTextY;
-          if (node.type !== 'start' && node.type !== 'end') {
-            bodyTextY = node.y + 25 + roundTo20(node.height - 20) / 2;
-          } else {
-            bodyTextY = node.y + 5 + roundTo20(node.height) / 2;
-          }
-          g.append('text').
-              attr('x', node.x + node.width / 2).
-              attr('y', bodyTextY).
-              attr('class', 'unselectable').
-              attr('text-anchor', 'middle').
-              text(function() {return text;}).each(function wrap() {
-            let self = d3.select(this),
-                textLength = self.node().getComputedTextLength(),
-                text = self.text();
-            while (textLength > (node.width - 2 * 4) && text.length > 0) {
-              text = text.slice(0, -1);
-              self.text(text + '...');
-              textLength = self.node().getComputedTextLength();
-            }
-          });
-        }
-      }
+        default: render,
+      },
     },
     data() {
       return {
