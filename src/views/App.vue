fc633fab480e13678b18dfc7c5e30a72bae1885d
izsl <iamppz@hotmail.com>
2020-07-11 17:32:12 +0800

    doc: add custom shape to demo

--- src/views/App.vue
+++ src/views/App.vue
@@ -175,49 +175,63 @@ export default {
       let borderColor = isSelected ? "#666666" : "#bbbbbb";
       if (node.type !== "start" && node.type !== "end") {
         // title
-        g.append("rect")
-          .attr("x", node.x)
-          .attr("y", node.y)
-          .attr("stroke", borderColor)
-          .attr("class", "title")
-          .style("height", "20px")
-          .style("fill", "#f1f3f4")
-          .style("stroke-width", "1px")
-          .style("width", node.width + "px");
-        g.append("text")
-          .attr("x", node.x + 4)
-          .attr("y", node.y + 15)
-          .attr("class", "unselectable")
-          .text(() => node.name)
-          .each(function wrap() {
-            let self = d3.select(this),
-              textLength = self.node().getComputedTextLength(),
-              text = self.text();
-            while (textLength > node.width - 2 * 4 && text.length > 0) {
-              text = text.slice(0, -1);
-              self.text(text + "...");
-              textLength = self.node().getComputedTextLength();
-            }
-          });
+        if (node.id !== 3) {
+          g.append("rect")
+            .attr("x", node.x)
+            .attr("y", node.y)
+            .attr("stroke", borderColor)
+            .attr("class", "title")
+            .style("height", "20px")
+            .style("fill", "#f1f3f4")
+            .style("stroke-width", "1px")
+            .style("width", node.width + "px");
+          g.append("text")
+            .attr("x", node.x + 4)
+            .attr("y", node.y + 15)
+            .attr("class", "unselectable")
+            .text(() => node.name)
+            .each(function wrap() {
+              let self = d3.select(this),
+                textLength = self.node().getComputedTextLength(),
+                text = self.text();
+              while (textLength > node.width - 2 * 4 && text.length > 0) {
+                text = text.slice(0, -1);
+                self.text(text + "...");
+                textLength = self.node().getComputedTextLength();
+              }
+            });
+        }
       }
       // body
-      let body = g.append("rect").attr("class", "body");
-      body
-        .style("width", node.width + "px")
-        .style("fill", "white")
-        .style("stroke-width", "1px");
-      if (node.type !== "start" && node.type !== "end") {
-        body.attr("x", node.x).attr("y", node.y + 20);
-        body.style("height", roundTo20(node.height - 20) + "px");
+      if (node.id === 3) {
+        let body = g.append("ellipse").attr("class", "body");
+        body.attr("cx", node.x + node.width / 2);
+        body.attr("cy", node.y + node.height / 2);
+        body.attr("rx", node.width / 2);
+        body.attr("ry", node.height / 2);
+        body.style("fill", "white");
+        body.style("stroke-width", "1px");
+        body.classed(node.type, true);
+        body.attr("stroke", borderColor);
       } else {
+        let body = g.append("rect").attr("class", "body");
         body
-          .attr("x", node.x)
-          .attr("y", node.y)
-          .classed(node.type, true)
-          .attr("rx", 30);
-        body.style("height", roundTo20(node.height) + "px");
+          .style("width", node.width + "px")
+          .style("fill", "white")
+          .style("stroke-width", "1px");
+        if (node.type !== "start" && node.type !== "end") {
+          body.attr("x", node.x).attr("y", node.y + 20);
+          body.style("height", roundTo20(node.height - 20) + "px");
+        } else {
+          body
+            .attr("x", node.x)
+            .attr("y", node.y)
+            .classed(node.type, true)
+            .attr("rx", 30);
+          body.style("height", roundTo20(node.height) + "px");
+        }
+        body.attr("stroke", borderColor);
       }
-      body.attr("stroke", borderColor);
 
       // body text
       let text =
@@ -232,7 +246,11 @@ export default {
           : node.approvers[0].name;
       let bodyTextY;
       if (node.type !== "start" && node.type !== "end") {
-        bodyTextY = node.y + 25 + roundTo20(node.height - 20) / 2;
+        if (node.id === 3) {
+          bodyTextY = node.y + 25;
+        } else {
+          bodyTextY = node.y + 25 + roundTo20(node.height - 20) / 2;
+        }
       } else {
         bodyTextY = node.y + 5 + roundTo20(node.height) / 2;
       }
