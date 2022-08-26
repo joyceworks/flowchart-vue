a9734369542a9e0933a5146a2a2c106e6dfebb6a
Michał Mrzygłód <mmrzyglod1@gmail.com>
2022-08-26 10:14:55 +0200

    Added possibility to pass slot content, slot context is excluded from mouse down and dbl click events

---
+++ src/utils/dom.js
@@ -0,0 +1,9 @@
+﻿function isMouseDownOnAnyChildElement(parentSelector, event) {
+    const parentElement = document.querySelector(parentSelector);
+    const childrenNodes = Array.from(parentElement.childNodes);
+    return childrenNodes.some((node) => node.contains(event.target));
+}
+
+export {
+    isMouseDownOnAnyChildElement
+};
